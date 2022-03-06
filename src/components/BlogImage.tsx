52a0af15116a1d68d2264829966c108019fc6bec
exxocism <tchoi.tab@gmail.com>
2022-03-06 13:44:11 +0900

    Feat: BlogImage (#3)

--- src/components/BlogImage.tsx
+++ src/components/BlogImage.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import DutyFree from '../assets/images/BlogImage.jpg';
+
 const BlogImage = () => {
-  return <ImageContainer></ImageContainer>;
+  return <ImageContainer src={DutyFree} alt="Duty Free" />;
 };
 
 const ImageContainer = styled.img`
-  height: 220px;
+  margin-top: 82px;
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
 `;
 
 export default BlogImage;
